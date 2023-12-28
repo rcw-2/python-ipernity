4b5abcbebb5a05bb88d48cdcd6f8b88222cff9b1
Ralph <91877177+rcw-2@users.noreply.github.com>
2023-12-28 19:33:01 +0100

    Test for specific errors

--- tests/test_errors.py
+++ tests/test_errors.py
@@ -1,12 +1,21 @@
 
 import pytest
 
-from ipernity import IpernityError
+from ipernity import APIRequestError, UnknownMethod
 
 
 def test_unknown_method(api):
-    with pytest.raises(IpernityError):
+    with pytest.raises(UnknownMethod):
+        api.call('unknown.method')
+    with pytest.raises(UnknownMethod):
         api.unknown.method()
     with pytest.raises(AttributeError):
         api._unknown.method()
+    with pytest.raises(AttributeError):
+        api.unknown._method()
+
+
+def test_api_request_error(api):
+    with pytest.raises(APIRequestError):
+        api.doc.get(doc_id = 1)
 
